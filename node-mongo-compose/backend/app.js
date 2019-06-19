a2924a77399dd645f56765042e243f38849cf99f
Renato Welinton Schlogel <rschlogel@hotmail.com>
2019-06-18 22:22:06 -0300

    atu

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,17 +2,29 @@ const express = require('express');
 const restful = require('node-restful');
 const server = express();
 const mongoose = restful.mongoose;
+const boryParser = require('body-parser');
+const cors = require('cors');
 
 // DataBase
-
 mongoose.Promise = global.Promise;
-
 mongoose.connect('mongodb://db/mydb');
 
-// teste
+// Middlewares
+server.use(boryParser.urlencoded({extended:true}));
+server.use(boryParser.json());
 
-server.get('/', (request, response, next) => response.send('BackEnd'));
+// ODM
+const Client = restful.model('Client', {
+    name:{type: String, required: true}
+});
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true});
+
+// Routes
+Client.register(server, '/clients');
+
 
 // startar server
-
 server.listen(3000);
